0c9a0643de20b4eb8fe4f2d810d8197782678552
sathvi185369 <161550338+sathvi185369@users.noreply.github.com>
2025-08-09 11:33:44 +0530

    Update settings.py

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'disease_prediction.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # if you have templates folder
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
